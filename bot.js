4c772b4ff9a6c2af5a5720d329a50ea4810d892f
abdullahcici68-ai <abdullahcici68@gmail.com>
2026-03-25 20:14:48 +0300

    bot duzeltildi

--- bot.js
+++ bot.js
@@ -1,11 +1,10 @@
+process.env.NODE_OPTIONS = '--max-old-space-size=256'
 const mineflayer = require('mineflayer')
 
 const HOST = 'Cicibaba38.aternos.me'
 const PORT = 54194
 const USERNAME = 'AFKBot'
 const VERSION = '1.21.11'
-
-let yenidenBaglanSayaci = 0
 
 function baslat() {
   const bot = mineflayer.createBot({
@@ -16,37 +15,32 @@ function baslat() {
     auth: 'offline'
   })
 
+  let hareketInterval = null
+
   bot.on('spawn', function() {
-    console.log('BAĞLANDI! AFK başladı.')
-    setInterval(function() {
+    console.log('BAĞLANDI!')
+    hareketInterval = setInterval(function() {
       bot.setControlState('jump', true)
       setTimeout(function() {
         bot.setControlState('jump', false)
       }, 500)
-      console.log('Hareket yapıldı')
     }, 30000)
   })
 
   bot.on('kicked', function(reason) {
     console.log('Atıldı: ' + reason)
-    yenidenBaglan()
+    if (hareketInterval) clearInterval(hareketInterval)
+    bot.end()
   })
 
   bot.on('error', function(err) {
     console.log('Hata: ' + err.message)
-    yenidenBaglan()
+    if (hareketInterval) clearInterval(hareketInterval)
   })
 
   bot.on('end', function() {
-    console.log('Bağlantı kesildi')
-    yenidenBaglan()
+    console.log('Bağlantı kesildi, 60 sn sonra tekrar denenecek...')
+    if (hareketInterval) clearInterval(hareketInterval)
+    setTimeout(baslat, 60000)
   })
 }
-
-function yenidenBaglan() {
-  yenidenBaglanSayaci++
-  console.log(yenidenBaglanSayaci + '. deneme - 5 sn sonra bağlanıyor...')
-  setTimeout(baslat, 15000)
-}
-
-baslat()
